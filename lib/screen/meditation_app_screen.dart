4930942431df176c0743500f8df3a63f67de9ac1
Vyacheslav Fedotov <vyacheslavfedotov@MacBook-Air-Vyacheslav.local>
2021-10-01 22:43:00 +0300

    final_version app

--- lib/screen/meditation_app_screen.dart
+++ lib/screen/meditation_app_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:meditations_app/items/meditation_item.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MeditationAppScreen extends StatefulWidget {
-  MeditationAppScreen({Key? key}) : super(key: key);
+  const MeditationAppScreen({Key? key}) : super(key: key);
 
   @override
   _MeditationAppScreenState createState() => _MeditationAppScreenState();
@@ -12,40 +14,80 @@ class _MeditationAppScreenState extends State<MeditationAppScreen> {
   final List<Item> items = [
     Item(
         itemName: "Forest",
-        audioPath: "meditation_audio/forest.mp3",
+        audioPath: "meditation_audios/forest.mp3",
         imagePath: "meditation_images/forest.jpeg"),
     Item(
         itemName: "Night",
-        audioPath: "meditation_audio/night.mp3",
+        audioPath: "meditation_audios/night.mp3",
         imagePath: "meditation_images/night.jpeg"),
     Item(
         itemName: "Ocean",
-        audioPath: "meditation_audio/ocean.mp3",
+        audioPath: "meditation_audios/ocean.mp3",
         imagePath: "meditation_images/ocean.jpeg"),
     Item(
         itemName: "Waterfall",
-        audioPath: "meditation_audio/waterfall.mp3",
+        audioPath: "meditation_audios/waterfall.mp3",
         imagePath: "meditation_images/waterfall.jpeg"),
     Item(
         itemName: "Wind",
-        audioPath: "meditation_audio/wind.mp3",
+        audioPath: "meditation_audios/wind.mp3",
         imagePath: "meditation_images/wind.jpeg"),
   ];
+
+  final AudioPlayer audioPlayer = AudioPlayer();
+
+  int? playingIndex;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Container(
-              width: 100,
-              child: ListTile(
-                title: Text(items[index].itemName),
-              ),
-            );
-          }),
-    ));
+      body: SafeArea(
+        child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(items[index].imagePath),
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Text(items[index].itemName),
+                    leading: IconButton(
+                      icon: playingIndex == index
+                          ? const FaIcon(FontAwesomeIcons.stop)
+                          : const FaIcon(FontAwesomeIcons.play),
+                      onPressed: () async {
+                        if (playingIndex == index) {
+                          playingIndex = null;
+                          setState(() {
+                            audioPlayer.stop();
+                          });
+                        } else {
+                          try {
+                            audioPlayer.setAsset(items[index].audioPath);
+                            await audioPlayer.play();
+                            setState(() {
+                              playingIndex = index;
+                            });
+                          } catch (error) {
+                            // ignore: avoid_print
+                            print(error);
+                          }
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
